61a3dd3dd98665781d5ec0dfe1a2a7135714a79c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-01 09:03:03 +0530

    RTCA

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   username: String,
   message: String,
+  room: String,
   timestamp: { type: Date, default: Date.now }
 });
 
