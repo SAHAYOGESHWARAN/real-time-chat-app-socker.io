77f43d16afbb2006cd42a01f582ea36dda92efa5
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-01 09:45:37 +0530

    RTCA

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -76,3 +76,27 @@ exports.login = async (req, res) => {
     res.status(500).send('Server error');
   }
 };
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+  res.status(201).json({ message: 'User registered successfully' });
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ message: 'User not found' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+};
